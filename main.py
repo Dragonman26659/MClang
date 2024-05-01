18e2457016268258d96e9b2171e870f9e26c9971
Dragonman26659 <77173319+Dragonman26659@users.noreply.github.com>
2024-05-01 10:31:46 +0000

    Refactor Python compiler structure

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-from PythonImpl.lexer import Lexer
-from PythonImpl.parser import Parser
-from PythonImpl.codegen import CodeGen
-
-fname = "test1.mcl"
-tempdir = "temp"
-buildDir = "build"
+from PythonImpl.Blang import *
 
 
-with open(fname) as f:
-    text_input = f.read()
-
-lexer = Lexer().get_lexer()
-tokens = lexer.lex(text_input)
-
-codegen = CodeGen()
-
-module = codegen.module
-builder = codegen.builder
-printf = codegen.printf
-
-pg = Parser(module, builder, printf)
-pg.parse()
-parser = pg.get_parser()
-parser.parse(tokens).eval()
-
-codegen.create_ir()
-codegen.save_ir(f"{tempdir}/output.ll")
